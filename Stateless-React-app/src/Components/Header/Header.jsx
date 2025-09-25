ea541eca92067d7a6fec1ba6f354f399238c235c
Kxppx <kappa.x@hotmail.com>
2025-09-25 18:02:11 -0500

    finished all tasks

---
+++ Stateless-React-app/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>Fullmetal Alchemist</h1>
+      <nav>
+        <li>about us</li>
+        <li>info</li>
+        <li>support us</li>
+      </nav>
+    </header>
+  );
+};
